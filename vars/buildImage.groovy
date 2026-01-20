bc6b74026c90b3adf466e110e192d3c7252d5d4c
EEslamo <141227368+EEslamo@users.noreply.github.com>
2026-01-20 07:44:01 +0200

    Fix typo in docker push command

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -5,6 +5,6 @@ def call() {
     withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t eeslamo003/jenkins-shared-library:jma-2.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker pusheeslamo003/jenkins-shared-library:jma-2.0'
+        sh 'docker push eeslamo003/jenkins-shared-library:jma-2.0'
     }
 }
